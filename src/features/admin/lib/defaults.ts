abb924acb720fecd2491575e5108b9bca7dbf279
kerntau <kerntau@outlook.com>
2026-03-18 11:48:23 +0800

    docs(comment): 汉化 src/features/admin/lib/defaults.ts 中的代码注释

--- src/features/admin/lib/defaults.ts
+++ src/features/admin/lib/defaults.ts
@@ -1,3 +1,6 @@
+/** 默认管理员用户名 */
 export const DEFAULT_ADMIN_USERNAME = 'kerntau'
+/** 默认管理员初始密码 (请务必登录后修改) */
 export const DEFAULT_ADMIN_PASSWORD = '123456'
+/** 管理员初始化状态存储 Key */
 export const ADMIN_BOOTSTRAP_STATE_KEY = 'admin.bootstrap.v1'
